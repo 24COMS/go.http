a3b1b106b82e7bc96c68cdfbf4b4584cfd081e2d
Borys Hulii <8tomat8@gmail.com>
2018-04-20 15:40:07 +0200

    Added GetLimitAndOffset helper

--- server/handler.go
+++ server/handler.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"net/http"
+	"net/url"
+	"strconv"
 
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -14,4 +17,27 @@ type BaseHTTPHandler struct {
 // NewLoggingResponseWriter creates new loggingResponseWriter instance for particular request
 func (h BaseHTTPHandler) NewLoggingResponseWriter(w http.ResponseWriter, r *http.Request) LoggingResponseWriter {
 	return newLoggingResponseWriter(h.Logger, r.RemoteAddr, r.Referer(), r.URL.Path, r.Method, w)
+}
+
+// GetLimitAndOffset will return parsed limit and offset from url.Values.
+func (BaseHTTPHandler) GetLimitAndOffset(values url.Values) (offset uint64, limit uint64, err error) {
+	rawOffset := values.Get("offset")
+	rawLimit := values.Get("limit")
+
+	if len(rawOffset) > 0 {
+		offset, err = strconv.ParseUint(rawOffset, 10, 64)
+		if err != nil {
+			err = errors.Wrap(err, "failed to parse offset")
+			return
+		}
+	}
+
+	if len(rawLimit) > 0 {
+		limit, err = strconv.ParseUint(rawLimit, 10, 64)
+		if err != nil {
+			err = errors.Wrap(err, "failed to parse limit")
+			return
+		}
+	}
+	return
 }
