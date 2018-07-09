f748c8a2bd86a370df23fd1fd7799669808da0e6
Borys Hulii <8tomat8@gmail.com>
2018-07-09 10:10:14 +0300

    Added common router error and updated deps

--- server/errors.go
+++ server/errors.go
@@ -4,7 +4,9 @@ import "errors"
 
 var (
 	// ErrHandlerConfigIsNotFull will be returned if some handlers Config fields are not populated
-	ErrHandlerConfigIsNotFull = errors.New("some handlers Config fields are not populated")
+	ErrHandlerConfigIsNotFull = errors.New("some handler Config fields are not populated")
 	// ErrServerConfigIsNotFull will be returned if some servers Config fields are not populated
-	ErrServerConfigIsNotFull = errors.New("some servers Config fields are not populated")
+	ErrServerConfigIsNotFull = errors.New("some server Config fields are not populated")
+	// ErrRouterConfigIsNotFull will be returned if some servers Config fields are not populated
+	ErrRouterConfigIsNotFull = errors.New("some router Config fields are not populated")
 )
